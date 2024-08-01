b441bfa8075a9419e1724a6af74fb4669f73a345
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-31 20:30:21 -0600

    Ejemplos de excepcion

--- ExcepcionesQ32024/Program.cs
+++ ExcepcionesQ32024/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+FileStream file = null;
+try
+{
+    file = File.Open("archivo.txt", FileMode.Open);
+    double n1, n2, division;
+
+    Console.WriteLine("Ingrese primer número:");
+    n1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese segundo número:");
+    n2 = double.Parse(Console.ReadLine());
+
+    division = n1 / n2;
+
+    Console.WriteLine($"La división es: {division}");
+} catch(FormatException e)
+{
+    Console.WriteLine(e.ToString());
+} catch(Exception e)
+{
+    Console.WriteLine(e.ToString()); 
+}
+finally
+{
+    file?.Close();
+    Console.WriteLine("Archivo cerrado...");
+}
